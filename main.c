5f24029d965fb3d2e8a4a764c437040b83b25e12
Edson Michaque <edson@michaque.com>
2023-04-06 23:07:00 +0200

    Format using BSD style

--- main.c
+++ main.c
@@ -1,58 +1,66 @@
-#include "thread.h"
-
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+#include "thread.h"
+
 void usage(void);
 void *worker(void *);
 
-int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    usage();
-  }
+int
+main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		usage();
+	}
 
-  int n_theard = atoi(argv[1]);
+	int n_theard = atoi(argv[1]);
 
-  pthread_t threads[n_theard];
-  thread_info_t threads_info[n_theard];
+	pthread_t threads[n_theard];
+	thread_info_t threads_info[n_theard];
 
-  for (int i = 0; i < n_theard; i++) {
-    threads_info[i] = (thread_info_t){
-        .id = i,
-        .sleep = i,
-    };
-  }
+	for (int i = 0; i < n_theard; i++) {
+		threads_info[i] = (thread_info_t) {
+			.id = i,
+			.sleep = i,
+		};
+	}
 
-  for (int i = 0; i < n_theard; i++) {
-    if (pthread_create(&threads[i], NULL, &worker, &threads_info[i])) {
-      perror("pthread_create");
-    }
-  }
+	for (int i = 0; i < n_theard; i++) {
+		if (pthread_create(&threads[i], NULL, &worker,
+			&threads_info[i])) {
+			perror("pthread_create");
+		}
+	}
 
-  for (int i = 0; i < n_theard; i++) {
-    if (pthread_join(threads[i], NULL)) {
-      perror("pthread_join");
-    }
-  }
+	for (int i = 0; i < n_theard; i++) {
+		if (pthread_join(threads[i], NULL)) {
+			perror("pthread_join");
+		}
+	}
 
-  return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
 
-void usage(void) {
-  fprintf(stderr, "main <threads>");
-  exit(EXIT_FAILURE);
+void
+usage(void)
+{
+	fprintf(stderr, "main <threads>");
+	exit(EXIT_FAILURE);
 }
 
-void *worker(void *args) {
-  thread_info_t *thread_info = (thread_info_t *)args;
+void *
+worker(void *args)
+{
+	thread_info_t *thread_info = (thread_info_t *)args;
 
-  for (int i = 0; i < 100; i++) {
-    printf("thread=%lu sleep=%d\n", (unsigned long int)pthread_self(), i);
-    sleep(thread_info->sleep);
-  }
+	for (int i = 0; i < 100; i++) {
+		printf("thread=%lu sleep=%d\n",
+		    (unsigned long int)pthread_self(), i);
+		sleep(thread_info->sleep);
+	}
 
-  return NULL;
+	return NULL;
 }
